6158197d7e07390bc6809606d17f9f2b0578cae3
Miguel Carneiro <mcarneiromorenas@gmail.com>
2024-05-04 21:23:13 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import markdoc from "@astrojs/markdoc";
-import mdx from "@astrojs/mdx";
-import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import markdoc from "@astrojs/markdoc"
+import mdx from "@astrojs/mdx"
+import robotsTxt from "astro-robots-txt"
 
-import icon from "astro-icon";
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mcarn.github.io',
-  compressHTML: true,
-	output: 'static'
-  integrations: [tailwind(), sitemap(), markdoc(), mdx(), robotsTxt(), icon()]
-});
+	site: "https://mcarn.github.io",
+	compressHTML: true,
+	output: "static",
+	integrations: [tailwind(), sitemap(), markdoc(), mdx(), robotsTxt(), icon()],
+})
