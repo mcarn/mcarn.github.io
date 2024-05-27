420ac6f19f04751bdcbfac5fd6af40f22f527bf2
Miguel Carneiro <mcarneiromorenas@gmail.com>
2024-05-27 16:19:50 +0100

    convert to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
-import markdoc from "@astrojs/markdoc"
 import mdx from "@astrojs/mdx"
 import robotsTxt from "astro-robots-txt"
 
@@ -12,5 +11,5 @@ export default defineConfig({
 	site: "https://mcarn.github.io",
 	compressHTML: true,
 	output: "static",
-	integrations: [tailwind(), sitemap(), markdoc(), mdx(), robotsTxt(), icon()],
+	integrations: [tailwind(), sitemap(), mdx(), robotsTxt(), icon()],
 })
