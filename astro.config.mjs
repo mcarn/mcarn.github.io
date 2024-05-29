20b6a0a274e2d0d6a88c4b400b3275c3a69d2d42
Miguel Carneiro <miguel.carneiro@imanage.com>
2024-05-29 08:36:16 +0100

    update package json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import sitemap from "@astrojs/sitemap"
-import mdx from "@astrojs/mdx"
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import robotsTxt from "astro-robots-txt";
+import icon from "astro-icon";
+import react from "@astrojs/react";
 
-import icon from "astro-icon"
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://mcarn.github.io",
-	compressHTML: true,
-	output: "static",
-	integrations: [tailwind(), sitemap(), mdx(), robotsTxt(), icon()],
-})
+  site: "https://mcarn.github.io",
+  compressHTML: true,
+  output: "static",
+  integrations: [tailwind(), sitemap(), mdx(), robotsTxt(), icon(), react(), compress()]
+});
