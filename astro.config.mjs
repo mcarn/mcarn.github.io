49481bd772f784fc214f6212c142a8a10bd6a5d5
Miguel Carneiro <mcarneiromorenas@gmail.com>
2025-05-01 13:48:16 +0100

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({  site: 'https://mcarn.github.io',});
+export default defineConfig({
+  site: 'https://mcarn.github.io',
+  integrations: [sitemap()],
+});
