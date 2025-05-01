dbb80bb453cbed55c07d45b61d020d5e7b7f409e
Miguel Carneiro <mcarneiromorenas@gmail.com>
2025-05-01 14:35:16 +0100

    added tailwind4 (#3)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
-
 import mdx from '@astrojs/mdx';
-
 import markdoc from '@astrojs/markdoc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mcarn.github.io',
   integrations: [sitemap(), mdx(), markdoc()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
