71b800d002daea1ef8edd4d8d9236ba9d9995bfa
Miguel Carneiro <mcarneiromorenas@gmail.com>
2025-05-01 14:26:28 +0100

    added mdx and markdoc (#2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import mdx from '@astrojs/mdx';
+
+import markdoc from '@astrojs/markdoc';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mcarn.github.io',
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx(), markdoc()],
 });
