04ffe4fb62d12f90c5735bf58536a2f54545d8a0
Miguel Carneiro <mcarneiromorenas@gmail.com>
2025-05-02 19:18:54 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import markdoc from '@astrojs/markdoc';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config"
+import sitemap from "@astrojs/sitemap"
+import mdx from "@astrojs/mdx"
+import markdoc from "@astrojs/markdoc"
+import tailwindcss from "@tailwindcss/vite"
+import playformCompress from "@playform/compress"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mcarn.github.io',
-  integrations: [sitemap(), mdx(), markdoc()],
+  site: "https://mcarn.github.io",
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      sourcemap: true,
+    },
   },
-});
+  integrations: [sitemap(), mdx(), markdoc(), playformCompress()],
+})
