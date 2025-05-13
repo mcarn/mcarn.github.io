6d24acaca918010c25b1d1e63b4d50af9d57e97b
Miguel Carneiro <mcarneiromorenas@gmail.com>
2025-05-13 11:11:48 +0100

    New (#7)
    * fix
    
    * update
    
    * fixed head
    
    * add pages
    
    * fixes
    
    * updates

---
+++ src/i18n/language.ts
@@ -0,0 +1,15 @@
+export const languages = {
+  es: "Español",
+  en: "English",
+}
+export const defaultLang = "es"
+export const ui = {
+  en: {
+    "head.title": "mcarn blog",
+    "head.description": "mcarn blog",
+  },
+  es: {
+    "head.title": "mcarn blog",
+    "head.description": "mcarn blog",
+  },
+} as const
---
+++ src/utils/constants.ts
@@ -0,0 +1,15 @@
+export const SITE_CONFIG = {
+  name: "mcarn blog",
+  title: "mcarn blog",
+  description: "mcarn blog",
+  author: "mcarn",
+  social: {
+    github: "https://github.com/mcarn",
+    twitter: "https://x.com/mcarnDev",
+  },
+  nav: [
+    { text: "Home", href: "/" },
+    { text: "Blog", href: "/blog" },
+    { text: "About", href: "/about" },
+  ],
+} as const
